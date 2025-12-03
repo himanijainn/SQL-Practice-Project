dc04ee0ca1d9187e5d9ee79df699fcc86a265a9b
Himani Jain <himanibjain@gmail.com>
2025-12-03 16:43:07 +0530

    storedprocedure_queries.sql

--- SQL_Queries/storedprocedure_queries.sql
+++ SQL_Queries/storedprocedure_queries.sql
@@ -9,3 +9,15 @@ on e.emp_id=s.emp_id
 where e.emp_id = emp_id;
 end//
 delimiter ;
+
+-- Create a stored procedure that returns all employees in a given department.
+delimiter //
+create procedure EmployeesAccordingToDepartments(in dept_id int)
+begin
+select concat(e.first_name,' ',e.last_name) as employee_name, d.dept_name
+from employees e
+join departments d
+on e.dept_id=d.dept_id
+where d.dept_id = dept_id;
+end//
+delimiter ;
